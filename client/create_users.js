164edea846253a5d8730c4776658f0faa80f06e9
Stepan Lopushansky <stepan.te.ua@gmail.com>
2015-10-20 01:19:02 +0300

    remove user

--- client/create_users.js
+++ client/create_users.js
@@ -35,6 +35,7 @@ function getData() {
             var fields = [], userData = [], index, element, user, formFields;
 
             index = $('.edit-user-button').index(this);
+            console.log(index);
             elementIndex = index;
             element = $('.edit-user-button').parent().parent()[index];
             user = $(element).context.children;
@@ -62,6 +63,21 @@ function getData() {
             };
             usersData.users[elementIndex] = data;
             $.ajax('/save', {method:'POST', json: true, data:JSON.stringify(usersData)});
+            elementIndex = null;
+        });
+
+        $('.remove-user-button').click(function (e) {
+            var index = $('.remove-user-button').index(this);
+            elementIndex = index;
+        });
+
+        $('.accept-remove-user').click(function (e) {
+            var el = $('#users tbody tr')[elementIndex];
+            usersData.users.splice(elementIndex, 1);
+            el.remove();
+            $.ajax('/save', {method:'POST', json: true, data:JSON.stringify(usersData)});
+            elementIndex = null;
+
         })
       });
 }
